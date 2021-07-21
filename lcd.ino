034107e9754459ac8088ce897cdba4f8161c2c28
scoutchorton <scoutchorton@yahoo.com>
2021-07-21 14:02:16 -0400

    Made a working Hello world, remember though to plug all your pins in!

--- lcd.ino
+++ lcd.ino
@@ -2,9 +2,9 @@
  * Macros
  */
 
-#define VO 53 //
-#define VO_INSTRUCTION LOW
-#define VO_DATA HIGH
+#define RS 53
+#define RS_INSTRUCTION LOW
+#define RS_DATA HIGH
 
 #define RW 52
 #define RW_READ HIGH
@@ -12,24 +12,44 @@
 
 #define ENABLE 51
 
-#define DATA_PIN(PIN_NUM) 30 + PIN_NUM
+#define DATA_PIN(PIN_NUM) 40 + PIN_NUM
 
-#define DISPLAY_STRING "Hello, world!"
+
+#define DISPLAY_STRING {'H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!'}
+
+
+#define TOGGLE_ENABLE() \
+    digitalWrite(ENABLE, HIGH); \
+    delay(1); \
+    digitalWrite(ENABLE, LOW);
+
+/**
+ * Defined types
+ */
 
 typedef unsigned char uint8;
+
+/**
+ * Code
+ */
 
 void writeToDataPins(uint8 data) {
     for(uint8 p_index = 0; p_index < 8; p_index++) {
       digitalWrite(DATA_PIN(p_index), data & (0b1 << p_index));
     }
-    digitalWrite(ENABLE, HIGH);
-    delay(1);
-    digitalWrite(ENABLE, LOW);
+
+    TOGGLE_ENABLE()
 }
 
 void setup() {
-  pinMode(VO, OUTPUT);
-  digitalWrite(VO, VO_INSTRUCTION);
+  char data[13] = DISPLAY_STRING;
+  
+  /**
+   * Pin setup
+   */
+  
+  pinMode(RS, OUTPUT);
+  digitalWrite(RS, RS_INSTRUCTION);
   
   pinMode(RW, OUTPUT);
   digitalWrite(RW, RW_WRITE);
@@ -39,18 +59,33 @@ void setup() {
 
   for(uint8 p_index = 0; p_index < 8; p_index++) 
     pinMode(DATA_PIN(p_index), OUTPUT);
+
+  //
+
+  /**
+   * Data writing
+   */
+
+  //Prepare for instructions
+  digitalWrite(RS, RS_INSTRUCTION);
+  digitalWrite(RW, RW_WRITE);
+  
+  //Initalize instruction set
+  writeToDataPins(0b00111000);
+
+  //Initalize display
+  writeToDataPins(0b00001100);
+  
+  //Clear display and move to beginning
+  writeToDataPins(0b00000001);
+
+  //Write string data
+  digitalWrite(RS, RS_DATA);
+  for(uint8 index = 0; index < sizeof(data); index++) {
+    delay(100);
+    writeToDataPins(data[index]);
+  }
 }
 
 void loop() {
-  //Move to beginning
-  digitalWrite(VO, VO_INSTRUCTION);
-  writeToDataPins(0b00000001);
-
-  /*
-  //Write string data
-  digitalWrite(VO, VO_DATA);
-  for(uint8 index = 0; index < sizeof(DISPLAY_STRING); index++) {
-    writeToDataPins(DISPLAY_STRING[index]);
-  }
-  */
 }
